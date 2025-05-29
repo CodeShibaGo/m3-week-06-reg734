b94cc89eeb197da723e73f1a452e47a53697fee7
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-29 16:50:18 +0800

    修正init 與config

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+from dotenv import  load_dotenv
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
