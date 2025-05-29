5b832e521be874fbabd256d78d1d7398c667102a
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-30 00:02:27 +0800

    raw sql 註冊頁完成

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,27 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from sqlalchemy import text
 
 app = Flask(__name__)
+login = LoginManager(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-with app.app_context():
-    raw_sql = """
-    CREATE TABLE IF NOT EXISTS users3 (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(64) NOT NULL,
-        email VARCHAR(120) NOT NULL UNIQUE,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    """
-    db.session.execute(text(raw_sql))
-    db.session.commit()
+# with app.app_context():
+#     raw_sql = """
+#     CREATE TABLE IF NOT EXISTS users3 (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         username VARCHAR(64) NOT NULL,
+#         email VARCHAR(120) NOT NULL UNIQUE,
+#         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#     )
+#     """
+#     db.session.execute(text(raw_sql))
+#     db.session.commit()
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import  login_user, logout_user, current_user, login_required
+from sqlalchemy import text
+import sqlalchemy as sa
+from app.models import User
+from urllib.parse import urlsplit
+from werkzeug.security import generate_password_hash
+
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Reg'}
     posts =[
@@ -17,11 +26,53 @@ def index():
         }
     ]
     return render_template('index.html', title='首頁', user=user, posts=posts)
+
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('使用者{} 的登入請求, 記住我={}'.format(
-            form.username.data, form.remember_me.data))
-        return  redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('無效的使用者名稱或密碼')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='登入', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+
+        sql = text("""
+            INSERT INTO user (username, email, password_hash)
+            VALUES (:username, :email, :password_hash)
+        """)
+
+        db.session.execute(sql, {
+            'username': form.username.data,
+            'email': form.email.data,
+            'password_hash': hashed_password
+        })
+        db.session.commit()
+
+        flash('恭喜，你現在是一名註冊使用者！')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
