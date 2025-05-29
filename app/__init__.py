caa0d4f6512581663fcd3c5c010f461ad80ac494
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-29 21:46:32 +0800

    sqlraw 添加

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,23 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+with app.app_context():
+    raw_sql = """
+    CREATE TABLE IF NOT EXISTS users3 (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(64) NOT NULL,
+        email VARCHAR(120) NOT NULL UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """
+    db.session.execute(text(raw_sql))
+    db.session.commit()
 
 from app import routes, models
