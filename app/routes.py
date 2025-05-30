aceb5a254045e433c5752a429d4a4aac61d43044
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-30 13:24:05 +0800

    加入個人頭像

--- app/routes.py
+++ app/routes.py
@@ -76,3 +76,13 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': '測試貼文 #1'},
+        {'author': user, 'body': '測試貼文 #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
