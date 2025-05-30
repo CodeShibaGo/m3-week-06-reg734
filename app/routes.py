aa212dd0be08aedbe15177183777885c0fa1cf82
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-30 14:04:56 +0800

    完成編輯頁面

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import  login_user, logout_user, current_user, login_required
 from sqlalchemy import text
 import sqlalchemy as sa
 from app.models import User
 from urllib.parse import urlsplit
 from werkzeug.security import generate_password_hash
+from datetime import datetime, timezone
 
 
 
@@ -86,3 +87,23 @@ def user(username):
         {'author': user, 'body': '測試貼文 #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
